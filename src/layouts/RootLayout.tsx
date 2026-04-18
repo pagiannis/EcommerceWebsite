5376e07c65f61e6dd542baa6aa0e6ad40a2a7e4c
pagiannis <pagiannis04@gmail.com>
2026-04-18 19:10:41 +0300

    Add scrolling to the top on refresh

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import AnnouncementBar from '../components/layout/AnnouncementBar';
-import Navbar from '../components/layout/Navbar';
-import Footer from '../components/layout/Footer';
-import NewsletterBanner from '../components/layout/NewsletterBanner';
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import AnnouncementBar from "../components/layout/AnnouncementBar";
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+import NewsletterBanner from "../components/layout/NewsletterBanner";
 
 export default function RootLayout() {
   return (
     <div className="flex min-h-screen flex-col">
+      <ScrollRestoration />
       <AnnouncementBar />
       <Navbar />
       <main className="flex-1">
