71f74984c784db6338bdde8c17011d93d3e276d1
angiorgos <angiorgos12@gmail.com>
2026-04-19 17:02:01 +0300

    removed autowired dependency.

--- server/src/main/java/com/ecommerce/server/controller/ProductController.java
+++ server/src/main/java/com/ecommerce/server/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.ecommerce.server.controller;
 
 import com.ecommerce.server.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
