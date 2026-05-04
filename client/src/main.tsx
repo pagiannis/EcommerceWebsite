dcb507a78c56160e9e31c8908f9ba34cc59a05f9
pagiannis <pagiannis04@gmail.com>
2026-05-04 18:33:13 +0300

    Setup ReactQueryDevtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import routes from "./routes";
 import { CartProvider } from "./context/CartContext";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,7 @@ createRoot(document.getElementById("root")!).render(
       <CartProvider>
         <RouterProvider router={routes} />
       </CartProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
