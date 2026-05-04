a2e035f64669b62411af2530628a54bc111c6841
pagiannis <pagiannis04@gmail.com>
2026-05-04 18:27:53 +0300

    Setup react-query and axios

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,23 @@ import "./index.css";
 import routes from "./routes";
 import { CartProvider } from "./context/CartContext";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60, // 1 minute
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CartProvider>
-      <RouterProvider router={routes} />
-    </CartProvider>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <RouterProvider router={routes} />
+      </CartProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
